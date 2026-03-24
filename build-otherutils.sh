800a0495d1efbe859a1ac82bc1d4cad1ac3ba26f
Matthew Yang <matthew.yang204@gmail.com>
2026-03-24 12:23:06 -0700

    Fix missing space

--- build-otherutils.sh
+++ build-otherutils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -d "nano"]; then
+if [ ! -d "nano" ]; then
     if [ ! -f "nano-8.7.1.tar.xz" ]; then
         wget https://nano-editor.org/dist/v8/nano-8.7.1.tar.xz
     fi
